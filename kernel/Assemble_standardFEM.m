988a288835c911d2256d3f549d17283b56741907
JingWeiWng <76798239+JingWeiWng@users.noreply.github.com>
2024-04-03 20:26:56 +0800

    Add files via upload

---
+++ kernel/Assemble_standardFEM.m
@@ -0,0 +1,117 @@
+function [solver,mesh]=Assemble_standardFEM(mesh,physic)
+
+k0=physic.k0;
+%guass points
+[xt,yt,pt,IntegralOrder]=GetGuassPoints(2);
+
+%init
+mesh.Dof=mesh.nbrVertex+mesh.nbrEdges;
+NbrNon=mesh.nbrTri*9;
+NumNonA=0;
+Ai=zeros(NbrNon,1);
+Aj=zeros(NbrNon,1);
+Av=zeros(NbrNon,1);
+NumNonB=0;
+Bi=zeros(NbrNon*4,1);
+Bj=zeros(NbrNon*4,1);
+Bv=zeros(NbrNon*4,1);
+
+%loop tri
+for n=1:mesh.nbrTri
+    %coordinate of tri's vertex
+    x=zeros(3,1);y=zeros(3,1);l=zeros(3,1);
+    x(1)=mesh.vertex(mesh.tri(n,1),1);y(1)=mesh.vertex(mesh.tri(n,1),2);
+    x(2)=mesh.vertex(mesh.tri(n,2),1);y(2)=mesh.vertex(mesh.tri(n,2),2);
+    x(3)=mesh.vertex(mesh.tri(n,3),1);y(3)=mesh.vertex(mesh.tri(n,3),2);
+    l(1)=sqrt((x(1)-x(2))*(x(1)-x(2))+(y(1)-y(2))*(y(1)-y(2)));
+    l(2)=sqrt((x(1)-x(3))*(x(1)-x(3))+(y(1)-y(3))*(y(1)-y(3)));
+    l(3)=sqrt((x(3)-x(2))*(x(3)-x(2))+(y(3)-y(2))*(y(3)-y(2)));
+
+    %jac matrix
+    Jac=zeros(3,3);
+    Jac(1,1)=x(2)-x(1);Jac(1,2)=y(2)-y(1);
+    Jac(2,1)=x(3)-x(1);Jac(2,2)=y(3)-y(1);Jac(3,3)=1;
+    InvJac=inv(Jac);
+    DetJac=abs(det(Jac));
+    DetJacx=det(Jac);
+    TJac=Jac'/DetJacx;
+
+    %bf
+    Et=zeros(3,3,IntegralOrder);curlEt=zeros(3,3,IntegralOrder);
+    Ez=zeros(3,3,IntegralOrder);gradEz=zeros(3,3,IntegralOrder);
+    temp=zeros(3,1);
+    for i=1:IntegralOrder
+        for j=1:3
+            [temp(1),temp(2)]=BF_Et(j,xt(i),yt(i));temp(3)=0;
+            Et(j,:,i)=InvJac*temp*l(j);
+            temp(3)=BF_curlEt(j,xt(i),yt(i));temp(1)=0;temp(2)=0;
+            curlEt(j,:,i)=TJac*temp*l(j);
+            temp(3)=BF_Ez(j,xt(i),yt(i));temp(1)=0;temp(2)=0;
+            Ez(j,:,i)=temp;
+            [temp(1),temp(2)]=BF_gradEz(j,xt(i),yt(i));temp(3)=0;
+            gradEz(j,:,i)=InvJac*temp;
+        end
+    end
+
+    %material
+    domain=mesh.triID(n);
+    epsilonr=physic.epsilonr(domain);
+    mu=physic.mur(domain);
+    
+    %submatrix
+    St=zeros(3,3);Tte=zeros(3,3);
+    Sz=zeros(3,3);Tz=zeros(3,3);
+    G=zeros(3,3);Ttu=zeros(3,3);
+    for i=1:3
+       for j=1:3
+          for k=1:IntegralOrder
+              St(i,j)=St(i,j)+pt(k)*DetJac/mu*dot(curlEt(i,:,k),curlEt(j,:,k));
+              Tte(i,j)=Tte(i,j)+pt(k)*DetJac*k0*k0*epsilonr*dot(Et(i,:,k),Et(j,:,k));
+              Sz(i,j)=Sz(i,j)+pt(k)*DetJac/mu*dot(gradEz(i,:,k),gradEz(j,:,k));
+              Tz(i,j)=Tz(i,j)+pt(k)*DetJac*k0*k0*epsilonr*dot(Ez(i,:,k),Ez(j,:,k));
+              G(i,j)=G(i,j)+pt(k)*DetJac/mu*dot(Et(i,:,k),gradEz(j,:,k));
+              Ttu(i,j)=Ttu(i,j)+pt(k)*DetJac/mu*dot(Et(i,:,k),Et(j,:,k));
+          end
+       end
+    end
+    Gt=G';
+    
+    for i=1:3
+        for j=1:3
+            MappingIndexVi=mesh.edgesOfTri(n,i)+mesh.nbrVertex;
+            MappingIndexVj=mesh.edgesOfTri(n,j)+mesh.nbrVertex;
+            MappingIndexSi=mesh.tri(n,i);
+            MappingIndexSj=mesh.tri(n,j);
+            
+            NumNonA=NumNonA+1;
+            Ai(NumNonA)=MappingIndexVi;
+            Aj(NumNonA)=MappingIndexVj;
+            Av(NumNonA)=St(i,j)-Tte(i,j);
+
+            NumNonB=NumNonB+1;
+            Bi(NumNonB)=MappingIndexVi;
+            Bj(NumNonB)=MappingIndexVj;
+            Bv(NumNonB)=Ttu(i,j);
+
+            NumNonB=NumNonB+1;
+            Bi(NumNonB)=MappingIndexSi;
+            Bj(NumNonB)=MappingIndexSj;
+            Bv(NumNonB)=Sz(i,j)-Tz(i,j);
+
+            NumNonB=NumNonB+1;
+            Bi(NumNonB)=MappingIndexVi;
+            Bj(NumNonB)=MappingIndexSj;
+            Bv(NumNonB)=G(i,j);
+
+            NumNonB=NumNonB+1;
+            Bi(NumNonB)=MappingIndexSi;
+            Bj(NumNonB)=MappingIndexVj;
+            Bv(NumNonB)=Gt(i,j);
+        end
+    end
+end
+
+solver.A = sparse(Ai,Aj,Av);
+solver.B = sparse(Bi,Bj,Bv);
+
+end
